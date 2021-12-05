62b57ef561d503c95a0e8de97de02b74737c2cc0
wuyanfeiying <wuyanfeiying@163.com>
2021-12-05 16:56:49 +0800

    登录注册-错误处理

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -14,7 +14,9 @@
           </p>
 
           <ul class="error-messages">
-            <li>That email is already taken</li>
+            <template v-for="(messages, field) in errors">
+              <li v-for="(message, index) in messages" :key="index">{{ field }} {{ message }}</li>
+            </template>
           </ul>
 
           <form @submit.prevent="onSubmit">
@@ -51,18 +53,24 @@ export default {
       user: {
         email: '',
         password: ''
-      }
+      },
+      errors: [] // 错误信息
     };
   },
 
   methods: {
     async onSubmit() {
-      // 提交表单请求登录
-      const { data } = await login({ user: this.user });
-      console.log(data);
-      // 保存用户的登陆状态
-      // 跳转到首页
-      this.$router.push('/');
+      try {
+        // 提交表单请求登录
+        const { data } = await login({ user: this.user });
+        console.log(data);
+        // 保存用户的登陆状态
+        // 跳转到首页
+        this.$router.push('/');
+      } catch (error) {
+        console.dir(error);
+        this.errors = error.response.data.errors;
+      }
     }
   }
 };
